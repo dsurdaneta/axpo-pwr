53cfe6796d679a1a9d001e60c5fade066a9718c2
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-08 23:19:49 +0200

    fix periodAsDateTime

--- PowerTrade/src/Pwr.App/Program.cs
+++ PowerTrade/src/Pwr.App/Program.cs
@@ -1,42 +1,42 @@
 ﻿using Axpo;
 
-namespace Pwr.App
+namespace Pwr.App;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        try
         {
-            try
-            {
-                IPowerService service = new PowerService();
-                var requestedUtc = DateTime.UtcNow;
-                var trades = await service.GetTradesAsync(requestedUtc);
-                Console.WriteLine($"Date Requested in UTC: {requestedUtc}");
-                Console.WriteLine($"Trades count: {trades.Count()}");
+            IPowerService service = new PowerService();
+            var requestedUtc = DateTime.UtcNow;
+            var trades = await service.GetTradesAsync(requestedUtc);
+            Console.WriteLine($"Date Requested in UTC: {requestedUtc}");
+            Console.WriteLine($"Trades count: {trades.Count()}");
 
-                var firstTrade = trades.FirstOrDefault();
-                var periodsCount = firstTrade.Periods.Count();
-                Console.WriteLine($"Periods count: {periodsCount}");
+            var firstTrade = trades.FirstOrDefault();
+            var periodsCount = firstTrade.Periods.Count();
+            Console.WriteLine($"Periods count: {periodsCount}");
 
-                for (int i = 0; i < firstTrade.Periods.Length; i++)
-                {
-                    var calculatedVolume = trades.Sum(t => t.Periods[i].Volume);
-                    var periodCounter = i + 1;
-                    var periodAsDateTime = new DateTime(requestedUtc.Year, requestedUtc.Month, requestedUtc.Day, requestedUtc.Hour + periodCounter, 0, 0);
-                    Console.WriteLine($"DateTime {periodAsDateTime} - Volume {calculatedVolume}");
-                }
-            }
-            catch (Axpo.PowerServiceException ex)
+            for (int i = 0; i < firstTrade.Periods.Length; i++)
             {
-                Console.WriteLine(ex.Message);
+                var calculatedVolume = trades.Sum(t => t.Periods[i].Volume);
+                var periodCounter = i + 1;
+                var auxDate = new DateTime(requestedUtc.Year, requestedUtc.Month, requestedUtc.Day, requestedUtc.Hour, 0, 0);
+                var periodAsDateTime = auxDate.AddHours(periodCounter);
+                Console.WriteLine($"{periodCounter} - DateTime {periodAsDateTime} - Volume {calculatedVolume}");
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            Console.WriteLine("Press any key");
-            Console.ReadLine();
         }
+        catch (Axpo.PowerServiceException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine("Press any key");
+        Console.ReadLine();
     }
 }
