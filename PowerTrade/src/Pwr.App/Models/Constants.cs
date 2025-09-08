464ade72f457ccb4b97b17e37d8aa1062001173b
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-08 23:52:36 +0200

    move constants and add dto for output

---
+++ PowerTrade/src/Pwr.App/Models/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Pwr.App.Models;
+
+internal static class Constants
+{
+    public const string FilePrefix = "PowerPosition";
+    public const string FileExtension = "csv";
+    public const string FileFormat = "YYYYMMDD_YYYYMMDDHHMM";
+}
