fa7690a1c07b9d0907d6a19073420f8ea389b41d
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-08 23:19:26 +0200

    add options and appsettings

---
+++ PowerTrade/src/Pwr.App/Options/CsvOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pwr.App.Options;
+
+public class CsvOptions
+{
+    public const string SectionName = "CsvOptions";
+    public const string FilePrefix = "PowerPosition";
+    public const string FileExtension = "csv";
+    public const string FileFormat = "YYYYMMDD_YYYYMMDDHHMM";
+
+    public string FolderPath { get; set; } = string.Empty;
+    public int ExtractIntervalMinutes { get; set; } = 15;
+}
