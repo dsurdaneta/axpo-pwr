c8742c4199c07c83d372a52c5d7ff573aeaa7888
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-08 23:53:13 +0200

    add separate method for requesting trades

--- PowerTrade/src/Pwr.App/Program.cs
+++ PowerTrade/src/Pwr.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Axpo;
+using Pwr.App.Models;
 
 namespace Pwr.App;
 
@@ -6,26 +7,40 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
+        var requestedUtc = DateTime.UtcNow.AddDays(1); // should request the forcast for the next day
+
+        IEnumerable<PowerTrade> trades = await GetTradesFromExternalService(requestedUtc);
+
+        var firstTrade = trades.FirstOrDefault();
+        var periodsCount = firstTrade.Periods.Count();
+        Console.WriteLine($"Periods count: {periodsCount}");
+
+        var rows = new List<OutputDto>();
+
+        for (int i = 0; i < firstTrade.Periods.Length; i++)
+        {
+            var calculatedVolume = trades.Sum(t => t.Periods[i].Volume);
+            var periodCounter = i + 1;
+            var auxDate = new DateTime(requestedUtc.Year, requestedUtc.Month, requestedUtc.Day, requestedUtc.Hour, 0, 0);
+            var periodAsDateTime = auxDate.AddHours(periodCounter);
+            Console.WriteLine($"{periodCounter} - DateTime {periodAsDateTime} - Volume {calculatedVolume}");
+            rows.Add(new OutputDto { DateTime = periodAsDateTime, Volume = calculatedVolume });
+        }
+
+        Console.WriteLine("Press any key");
+        Console.ReadLine();
+    }
+
+    private static async Task<IEnumerable<PowerTrade>> GetTradesFromExternalService(DateTime requestedUtc)
+    {
+        var trades = Enumerable.Empty<PowerTrade>();
         try
         {
-            var requestedUtc = DateTime.UtcNow.AddDays(1); // should request the forcast for the next day
-            IPowerService service = new PowerService();            
-            var trades = await service.GetTradesAsync(requestedUtc);
+            IPowerService service = new PowerService();
+            trades = await service.GetTradesAsync(requestedUtc);
             Console.WriteLine($"Date Requested in UTC: {requestedUtc}");
             Console.WriteLine($"Trades count: {trades.Count()}");
-
-            var firstTrade = trades.FirstOrDefault();
-            var periodsCount = firstTrade.Periods.Count();
-            Console.WriteLine($"Periods count: {periodsCount}");
-
-            for (int i = 0; i < firstTrade.Periods.Length; i++)
-            {
-                var calculatedVolume = trades.Sum(t => t.Periods[i].Volume);
-                var periodCounter = i + 1;
-                var auxDate = new DateTime(requestedUtc.Year, requestedUtc.Month, requestedUtc.Day, requestedUtc.Hour, 0, 0);
-                var periodAsDateTime = auxDate.AddHours(periodCounter);
-                Console.WriteLine($"{periodCounter} - DateTime {periodAsDateTime} - Volume {calculatedVolume}");
-            }
+            return trades;
         }
         catch (Axpo.PowerServiceException ex)
         {
@@ -36,7 +51,6 @@ internal class Program
             Console.WriteLine(ex.Message);
         }
 
-        Console.WriteLine("Press any key");
-        Console.ReadLine();
+        return trades;
     }
 }
