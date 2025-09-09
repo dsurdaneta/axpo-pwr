55e0c6b680886114cf46e63adb86bc6dd62239e4
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-09 20:54:01 +0200

    rename service and add interface

---
+++ PowerTrade/src/Pwr.App/Services/ForcastCallingService.cs
@@ -0,0 +1,69 @@
+﻿using Axpo;
+using Microsoft.Extensions.Logging;
+using Pwr.App.Models;
+
+namespace Pwr.App.Services;
+
+public class ForcastCallingService : IForcastCallingService
+{
+    private readonly ILogger<ForcastCallingService> _logger;
+    private readonly IPowerService _powerService;
+
+    public ForcastCallingService(ILogger<ForcastCallingService> logger, IPowerService powerService)
+    {
+        _logger = logger;
+        _powerService = powerService;
+    }    
+
+    public async Task<List<OutputItemDto>> GetForcastAsync(DateTime requestedUtc)
+    {
+        var rows = new List<OutputItemDto>();
+        var trades = await GetTradesFromExternalServiceAsync(requestedUtc);
+        if (!trades.Any())
+        {
+            _logger.LogWarning("No trades retrieved for date {RequestedDate}", requestedUtc);
+            return rows;
+        }
+
+        var periodsCount = trades.Min(t => t.Periods.Length);
+        _logger.LogInformation("Periods count: {PeriodsCount} for date {RequestedDate}", periodsCount, requestedUtc);
+
+        for (int i = 0; i < periodsCount; i++)
+        {
+            var calculatedVolume = trades.Sum(t => t.Periods[i].Volume);
+            var periodCounter = i + 1;
+            var auxDate = new DateTime(requestedUtc.Year, requestedUtc.Month, requestedUtc.Day, requestedUtc.Hour, 0, 0);
+            var periodAsDateTime = auxDate.AddHours(periodCounter);
+            rows.Add(new OutputItemDto { DateTime = periodAsDateTime, Volume = calculatedVolume });
+        }
+
+        return rows;
+    }
+
+    internal async Task<IEnumerable<PowerTrade>> GetTradesFromExternalServiceAsync(DateTime requestedUtc)
+    {
+        //TODO 
+        //Read from config
+        //DST validation
+        //Dependency injection
+        _logger.LogInformation("Starting to get trades from external service for date {RequestedDate}", requestedUtc);
+
+        IEnumerable<PowerTrade> trades = new List<PowerTrade>();
+
+        try
+        {
+            trades = await _powerService.GetTradesAsync(requestedUtc);
+            _logger.LogInformation("Successfully retrieved {TradeCount} trades from external service", trades.Count());
+        }
+        catch (Axpo.PowerServiceException ex)
+        {
+            _logger.LogError(ex, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while retrieving trades from external service");
+        }
+
+        return trades;
+    }
+}
---
+++ PowerTrade/src/Pwr.App/Services/IForcastCallingService.cs
@@ -0,0 +1,8 @@
+﻿using Pwr.App.Models;
+
+namespace Pwr.App.Services;
+
+public interface IForcastCallingService
+{
+    Task<List<OutputItemDto>> GetForcastAsync(DateTime requestedUtc);
+}
