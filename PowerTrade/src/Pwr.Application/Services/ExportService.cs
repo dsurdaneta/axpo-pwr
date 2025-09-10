a9a9cb538bef88e2ba7fc4ce16432d6ae9ed6676
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-10 12:57:27 +0200

    use base ctor in services, update comments

--- PowerTrade/src/Pwr.Application/Services/ExportService.cs
+++ PowerTrade/src/Pwr.Application/Services/ExportService.cs
@@ -8,38 +8,31 @@ using Pwr.Application.Interfaces;
 
 namespace Pwr.Application.Services;
 
-public class ExportService : IExportService
+public class ExportService(ILogger<ExportService> logger) : IExportService
 {
     private const string CsvDelimiter = ";";
     private const string FilePrefix = "PowerPosition";
     private const string FileFormat = "yyyyMMdd_yyyyMMddHHmm";
 
-    private readonly ILogger<ExportService> _logger;
-
-    public ExportService(ILogger<ExportService> logger)
-    {
-        _logger = logger;
-    }
-
     public bool GenerateReport(DateTime requestedUtc, List<OutputItemDto> rows)
     {
         if (rows == null || rows.Count == 0)
         {
-            _logger.LogWarning("No data available to generate report for date {RequestedDate}", requestedUtc);
+            logger.LogWarning("No data available to generate report for date {RequestedDate}", requestedUtc);
             return false;
         }
-
-        var datePart = requestedUtc.ToString(FileFormat);
-        var fileName = $"{FilePrefix}_{datePart}.csv";
+        
         var basePath = $"C:\\Users\\DanielUrdanetaOropez\\source";
-        _logger.LogInformation("Starting to write CSV report for date {RequestedDate}", requestedUtc);
+        logger.LogInformation("Starting to write CSV report for date {RequestedDate}", requestedUtc);
 
         try
         {
-            //TODO create output folder if does not exists
-            //Delimiter as constant
+            //TODO
+            //create output folder if does not exists
             //Read from config
-            //implement interface
+            //if config valu is null use constant defalt
+            var datePart = requestedUtc.ToString(FileFormat);
+            var fileName = $"{FilePrefix}_{datePart}.csv";
 
             var config = new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = CsvDelimiter, Encoding = Encoding.UTF8 };
 
@@ -49,12 +42,12 @@ public class ExportService : IExportService
                 csv.WriteRecords(rows);
             }
 
-            _logger.LogInformation("CSV report {FileName} created successfully", fileName);
+            logger.LogInformation("CSV report {FileName} created successfully", fileName);
             return true;
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Error occurred while generating CSV report for date {RequestedDate}", requestedUtc);
+            logger.LogError(ex, "Error occurred while generating CSV report for date {RequestedDate}", requestedUtc);
             return false;
         }
     }
--- PowerTrade/src/Pwr.Application/Services/ForcastCallingService.cs
+++ PowerTrade/src/Pwr.Application/Services/ForcastCallingService.cs
@@ -5,29 +5,22 @@ using Pwr.Domain.Models;
 
 namespace Pwr.Application.Services;
 
-public class ForcastCallingService : IForcastCallingService
+public class ForcastCallingService(ILogger<ForcastCallingService> logger, IPowerService powerService) : IForcastCallingService
 {
-    private readonly ILogger<ForcastCallingService> _logger;
-    private readonly IPowerService _powerService;
-
-    public ForcastCallingService(ILogger<ForcastCallingService> logger, IPowerService powerService)
-    {
-        _logger = logger;
-        _powerService = powerService;
-    }
-
     public async Task<IEnumerable<OutputItemDto>> GetForcastAsync(DateTime requestedUtc)
     {
+        //TODO 
+        //DST validation
         var rows = new List<OutputItemDto>();
         var trades = await GetTradesFromExternalServiceAsync(requestedUtc);
         if (!trades.Any())
         {
-            _logger.LogWarning("No trades retrieved for date {RequestedDate}", requestedUtc);
+            logger.LogWarning("No trades retrieved for date {RequestedDate}", requestedUtc);
             return rows;
         }
 
         var periodsCount = trades.Min(t => t.Periods.Length);
-        _logger.LogInformation("Periods count: {PeriodsCount} for date {RequestedDate}", periodsCount, requestedUtc);
+        logger.LogInformation("Periods count: {PeriodsCount} for date {RequestedDate}", periodsCount, requestedUtc);
 
         for (int i = 0; i < periodsCount; i++)
         {
@@ -38,33 +31,29 @@ public class ForcastCallingService : IForcastCallingService
             rows.Add(new OutputItemDto { DateTime = periodAsDateTime, Volume = calculatedVolume });
         }
 
-        _logger.LogInformation("Rows {RowsCount} generated for {RequestedDate}", rows.Count, requestedUtc);
+        logger.LogInformation("Rows {RowsCount} generated for {RequestedDate}", rows.Count, requestedUtc);
 
         return rows;
     }
 
     internal async Task<IEnumerable<PowerTrade>> GetTradesFromExternalServiceAsync(DateTime requestedUtc)
-    {
-        //TODO 
-        //Read from config
-        //DST validation
-        //Dependency injection
-        _logger.LogInformation("Starting to get trades from external service for date {RequestedDate}", requestedUtc);
+    {        
+        logger.LogInformation("Starting to get trades from external service for date {RequestedDate}", requestedUtc);
 
         IEnumerable<PowerTrade> trades = new List<PowerTrade>();
 
         try
         {
-            trades = await _powerService.GetTradesAsync(requestedUtc);
-            _logger.LogInformation("Successfully retrieved {TradeCount} trades from external service", trades.Count());
+            trades = await powerService.GetTradesAsync(requestedUtc);
+            logger.LogInformation("Successfully retrieved {TradeCount} trades from external service", trades.Count());
         }
         catch (Axpo.PowerServiceException ex)
         {
-            _logger.LogError(ex, ex.Message);
+            logger.LogError(ex, ex.Message);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Error occurred while retrieving trades from external service");
+            logger.LogError(ex, "Error occurred while retrieving trades from external service");
         }
 
         return trades;
