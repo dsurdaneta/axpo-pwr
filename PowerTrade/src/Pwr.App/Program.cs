36c259cf194c1b1b0dee1d874f561740f3f597c9
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-10 12:36:10 +0200

    rename to Pwr.ConsoleApp

--- PowerTrade/src/Pwr.App/Program.cs
+++ PowerTrade/src/Pwr.App/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Pwr.App;
+namespace Pwr.ConsoleApp;
 
 internal class Program
 {
