341bd826a039f5bcb95770e99a3ab1953bfe61a1
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-12 16:27:40 +0200

    add timer service tests

---
+++ PowerTrade/test/Pwr.UnitTests/Application/TimerServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Pwr.Application.Services;
+using Shouldly;
+
+namespace Pwr.UnitTests.Application;
+
+public class TimerServiceTests
+{
+    private readonly TimerService _timerService = new(NullLogger<TimerService>.Instance);
+
+    [Fact]
+    public void Start_TimerStartsSuccessfully()
+    {
+        // Arrange
+        var callbackInvoked = false;
+        Func<Task> callback = () =>
+        {
+            callbackInvoked = true;
+            return Task.CompletedTask;
+        };
+
+        // Act
+        _timerService.Start(100, callback);
+        Task.Delay(200).Wait(); // Wait to allow timer to trigger
+
+        // Assert
+        _timerService.Stop();
+        callbackInvoked.ShouldBeTrue();
+    }
+}
