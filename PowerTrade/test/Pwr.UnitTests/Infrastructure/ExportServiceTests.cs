4a68935f0faa3dfb5527ea268f394324af710763
Daniel Urdaneta <5184296+dsurdaneta@users.noreply.github.com>
2025-09-12 15:34:47 +0200

    add exportservice tests

---
+++ PowerTrade/test/Pwr.UnitTests/Infrastructure/ExportServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Pwr.Infrastructure.Options;
+using Pwr.Infrastructure.Services;
+using NSubstitute;
+using Pwr.Domain.Models;
+using Shouldly;
+
+namespace Pwr.UnitTests.Infrastructure;
+
+public class ExportServiceTests
+{
+    [Fact]
+    public void GenerateReport_WhenRowsAreEmpty_ShouldReturnFalse()
+    {
+        // Arrange
+        var loggerMock = NullLogger<ExportService>.Instance;
+        IOptionsMonitor<CsvOptions> optionsMock = Substitute.For<IOptionsMonitor<CsvOptions>>();
+        optionsMock.CurrentValue.Returns(new CsvOptions { FolderPath = "SomePath" });
+
+        var exportService = new ExportService(loggerMock, optionsMock);
+        var requestedUtc = DateTime.UtcNow;
+        var emptyRows = new List<InputItemDto>();
+
+        // Act
+        var result = exportService.GenerateReport(requestedUtc, emptyRows);
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+}
